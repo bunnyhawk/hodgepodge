0a25d300b3e472a913a25f11d29ed14c9c649f32
Robert Higdon <robert@ulastnight.com>
2022-08-25 22:34:40 -0700

    Testing local api

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,7 +63,7 @@ const submitForm = (e) => {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(formDataValues)
-  }).then(() => location.href = 'https://mailthis.to/confirm');
+  }).then(() => location.href = '/api/submit');
 }
 
 const formSubmitButton = document.getElementById('submit-form');
